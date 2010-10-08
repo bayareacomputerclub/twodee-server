f1e2141f383be174126d7d7878466329b18aae09
Kristofer Joseph <kristofer@kristoferjoseph.com>
2010-10-07 20:10:46 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwoDee::Application.config.secret_token = 'c2572b9263551d1cd60e9f5ab7555d0f1eb924113c0b7ff144adafb6d5fbc9c67f6c2d0fcacd2b572d6282fa309e03571e66c5a84149368c9b4cf504a11042dd'
